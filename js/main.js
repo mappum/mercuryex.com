63629dfbce341ef170624fd404e02ed82abf4c44
Matt Bell <mappum@gmail.com>
2015-03-04 21:59:49 -0800

    Cleaned up platform detection code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var PATH = 'https://github.com/mappum/mercury/releases/download/0.0.1-alpha/MercuryWallet-0.0.1-SNAPSHOT.';
 
-function getPlatform() {
-  var p = window.navigator.platform.toLowerCase();
+function getPlatform(platform) {
+  var p = platform.toLowerCase();
   if(p.indexOf('win') === 0) return 'windows';
   else if(p.indexOf('mac') === 0) return 'apple';
   // TODO: mobile
 }
 
-function getFiletype() {
+function getFiletype(platform) {
   var suffix = '';
-  var p = window.navigator.platform.toLowerCase();
+  var p = platform.toLowerCase();
   if(p.indexOf('win') === 0) suffix = 'exe';
   else if(p.indexOf('mac') === 0) suffix = 'dmg';
   else suffix = 'jar';
@@ -18,13 +18,18 @@ function getFiletype() {
 }
 
 $(function() {
-  var platform = getPlatform();
-  var suffix = getFiletype();
+  var platform = getPlatform(window.navigator.platform);
+  var suffix = getFiletype(window.navigator.platform);
 
   $('.download-button').each(function(i, el) {
     el = $(el);
-    var filename = PATH + (el.attr('data-suffix') || suffix);
-    el.attr('href', filename)
-      .find('> .platform-icon').addClass('fa-' + platform);
+
+    var s = suffix;
+    if(el.attr('data-platform')) s = getFiletype(el.attr('data-platform'));
+    el.attr('href', PATH + s);
+
+    var p = platform;
+    if(el.attr('data-platform')) p = getPlatform(el.attr('data-platform'));
+    el.find('> .platform-icon').addClass('fa-' + p);
   });
 });
